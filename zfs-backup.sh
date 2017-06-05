tobox/zfs-backup

commit b2a33287f00797b262b010e078e84055e665b5df

Multiple FS/vol support, first try.  Currently broken in that the while read loop may terminate too soon.
Also support settings in a .cfg file.

zfs-backup.sh
@@ -5,6 +5,7 @@
 # server via zfs snapshots and zfs send/receive
 #
 # SMF manifests welcome!
+# v0.2 - multiple datasets
 
 # Copyright (c) 2009-12 Andrew Daugherity <adaugherity@tamu.edu>
 # All rights reserved.
@@ -53,18 +54,98 @@
 #   * zfs send incremental (-I) from $newest_remote to $latest_local to dsthost
 #   * if anything fails, set svc to maint. and exit
 
-DEBUG=""		# set to non-null to enable debug (dry-run)
-VERBOSE=""		# "-v" for verbose, null string for quiet
+DEBUG="1"		# set to non-null to enable debug (dry-run)
+VERBOSE="-v"		# "-v" for verbose, null string for quiet
 LOCK="/var/tmp/zfsbackup.lock"
+CFG="./zfs-backup.cfg"
 
-# local pool/fs settings
-POOL="mypool"
-FS="myfs"
-TAG="zfs-auto-snap_hourly"
+# local settings -- datasets to back up are now found by property
+TAG="zfs-auto-snap_daily"
+PROP="edu.tamu:backuptarget"
 # remote settings (on destination host)
 REMUSER="zfsbak"
 REMHOST="backupserver.my.domain"
 REMPOOL="backuppool"
+
+# Read any settings from a config file, if present
+if [ -r $CFG ]; then
+    . $CFG
+fi
+
+# Usage: do_backup pool/fs/to/backup receive_option
+#   receive_option should be -d for full path and -e for base name
+#   See the descriptions in the 'zfs receive' section of zfs(1M) for more details.
+do_backup() {
+
+    DATASET=$1
+    FS=${DATASET#*/}		# strip local pool name
+    FS_BASE=${DATASET##*/}	# only the last part
+    RECV_OPT=$2
+
+    case $RECV_OPT in
+	-e)	TARGET="$REMPOOL/$FS_BASE"
+		;;
+	-d)	TARGET="$REMPOOL/$FS"
+		;;
+	*)	BAD=1
+    esac
+    if [ $# -ne 2 -o "$BAD" ]; then
+	echo "Oops! do_backup called improperly:" 1>&2
+	echo "    $*" 1>&2
+	return 2
+    fi
+
+    newest_local="$(/usr/sbin/zfs list -t snapshot -H -S creation -o name -d 1 $DATASET | grep $TAG | head -1)"
+    snap2=${newest_local#*@}
+    [ "$DEBUG" -o "$VERBOSE" ] && echo "newest local snapshot: $snap2"
+
+    # needs public key auth configured beforehand
+    newest_remote="$(ssh $REMUSER@$REMHOST /usr/sbin/zfs list -t snapshot -H -S creation -o name -d 1 $TARGET | grep $TAG | head -1)"
+    if [ -z $newest_remote ]; then
+	echo "Error fetching remote snapshot listing via ssh to $REMUSER@$REMHOST." >&2
+	[ $DEBUG ] || touch $LOCK
+	return 0
+    fi
+    snap1=${newest_remote#*@}
+    [ "$DEBUG" -o "$VERBOSE" ] && echo "newest remote snapshot: $snap1"
+
+    if ! /usr/sbin/zfs list -t snapshot -H $DATASET@$snap1 > /dev/null 2>&1; then
+	exec 1>&2
+	echo "Newest remote snapshot '$snap1' does not exist locally!"
+	echo "Perhaps it has been already rotated out."
+	echo ""
+	echo "Manually run zfs send/recv to bring $TARGET on $REMHOST"
+	echo "to a snapshot that exists on this host (newest local snapshot with the"
+	echo "tag $TAG is $snap2)."
+	[ $DEBUG ] || touch $LOCK
+	return 1
+    fi
+return 0
+    if ! /usr/sbin/zfs list -t snapshot -H $DATASET@$snap2 > /dev/null 2>&1; then
+	exec 1>&2
+	echo "Something has gone horribly wrong -- local snapshot $snap2"
+	echo "has suddenly disappeared!"
+	[ $DEBUG ] || touch $LOCK
+	return 1
+    fi
+
+    if [ "$snap1" = "$snap2" ]; then
+	[ $VERBOSE ] && echo "Remote snapshot is the same as local; not running."
+	return 0
+    fi
+
+    if [ $DEBUG ]; then
+	echo "would run: /usr/sbin/zfs send -R -I $snap1 $DATASET@$snap2 |"
+	echo "  ssh $REMUSER@$REMHOST /usr/sbin/zfs recv $RECV_OPT -vF $REMPOOL"
+    else
+	if ! /usr/sbin/zfs send -R -I $snap1 $DATASET@$snap2 | \
+	  ssh $REMUSER@$REMHOST /usr/sbin/zfs recv $VERBOSE $RECV_OPT -F $REMPOOL; then
+	    echo 1>&2 "Error sending snapshot."
+	    touch $LOCK
+	    return 1
+	fi
+    fi
+}
 
 # begin main script
 if [ -e $LOCK ]; then
@@ -90,53 +171,34 @@ if [ "$1" = "-v" ]; then
     VERBOSE=$1
 fi
 
-newest_local="$(/usr/sbin/zfs list -t snapshot -H -S creation -o name -r $POOL/$FS | grep "$FS@$TAG" | head -1)"
-snap2=${newest_local#*@}
-[ "$DEBUG" -o "$VERBOSE" ] && echo "newest local snaphost: $snap2"
+FAIL=0
+# get the datasets that have our backup property set
+/usr/sbin/zfs get -s local -H -o name,value $PROP | \
+while read dataset value
+do
+    case $value in
+	fullpath) [ $VERBOSE ] && echo "\n$dataset:"
+	    do_backup $dataset -d
+		;;
+	basename) [ $VERBOSE ] && echo "\n$dataset:"
+	    do_backup $dataset -e
+		;;
+	*)  echo "Warning: $dataset has invalid backuptarget property '$value', skipping." >&2
+		;;
+    esac
+    STATUS=$?
+    if [ $STATUS -gt 0 ]; then
+	FAIL=$(($FAIL | $STATUS))
+    fi
+done
 
-# needs public key auth configured beforehand
-newest_remote="$(ssh $REMUSER@$REMHOST /usr/sbin/zfs list -t snapshot -H -S creation -o name -r $REMPOOL/$FS | grep "$FS@$TAG" | head -1)"
-if [ -z $newest_remote ]; then
-    echo 1>&2 "Error fetching remote snapshot listing via ssh to $REMUSER@$REMHOST."
-    touch $LOCK
-    exit 1
-fi
-snap1=${newest_remote#*@}
-[ "$DEBUG" -o "$VERBOSE" ] && echo "newest remote snaphost: $snap1"
-
-if ! /usr/sbin/zfs list -t snapshot -H $POOL/$FS@$snap1 > /dev/null 2>&1; then
-    exec 1>&2
-    echo "Newest hourly remote snapshot '$snap1' does not exist locally!"
-    echo "Perhaps it has been already rotated out."
-    echo ""
-    echo "Manually run zfs send/recv to bring $REMPOOL/$FS"
-    echo "on $REMHOST to a snapshot that exists on this host (newest local hourly"
-    echo "snapshot is $snap2)."
-    touch $LOCK
-    exit 1
-fi
-if ! /usr/sbin/zfs list -t snapshot -H $POOL/$FS@$snap2 > /dev/null 2>&1; then
-    exec 1>&2
-    echo "Something has gone horribly wrong -- local snapshot $snap2"
-    echo "has suddenly disappeared!"
-    touch $LOCK
-    exit 1
-fi
-
-if [ "$snap1" = "$snap2" ]; then
-    [ $VERBOSE ] && echo "Remote snapshot is the same as local; not running."
-    exit 0
-fi
-
-if [ $DEBUG ]; then
-    echo "would run: /usr/sbin/zfs send -R -I $snap1 $POOL/$FS@$snap2 |"
-    echo "  ssh $REMUSER@$REMHOST /usr/sbin/zfs recv -dvF $REMPOOL"
-else
-    if ! /usr/sbin/zfs send -R -I $snap1 $POOL/$FS@$snap2 | \
-      ssh $REMUSER@$REMHOST /usr/sbin/zfs recv $VERBOSE -dF $REMPOOL; then
-        echo 1>&2 "Error sending snapshot."
-        touch $LOCK
-        exit 1
+if [ $FAIL -gt 0 ]; then
+    if [ $(($FAIL & 1)) -gt 0 ]; then
+	echo "There were errors backing up some datasets." >&2
+    fi
+    if [ $(($FAIL & 2)) -gt 0 ]; then
+	echo "Some datasets had misconfigured $PROP properties." >&2
     fi
 fi
 
+exit $FAIL

.gitignore
@@ -1,0 +1,1 @@
+zfs-backup.cfg

commit 8184528b03d60fb68a04e40dd932543af7a8df01

Fixes bug introduced in b2a3328, because while loops get executed in a subshell. Not every copy of Bash has lastpipe enabled. See http://mywiki.wooledge.org/BashFAQ/024.

zfs-backup.sh
@@ -291,7 +291,6 @@ if [ $COUNT -lt 1 ]; then
     rm $PID
     exit 2
 fi
-$ZFS get -s local -H -o name,value $PROP |
 while read dataset value
 do
     case $value in
@@ -319,7 +318,7 @@ do
     if [ $STATUS -gt 0 ]; then
 	FAIL=$((FAIL | STATUS))
     fi
-done
+done < < ($ZFS get -s local -H -o name,value $PROP )
 
 if [ $FAIL -gt 0 ]; then
     if [ $((FAIL & 1)) -gt 0 ]; then